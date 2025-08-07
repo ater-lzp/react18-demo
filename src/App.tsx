8ef58e050e009a4bff5214965de0fac9f6bc2f58
ater <2083642787@qq.com>
2025-08-07 09:19:03 +0800

    build: 添加 Tailwind CSS 样式表
    - 在 index.html 中添加了 tailwind.css 样式表引用
    - 此修改将有助于实现项目的样式和布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,42 @@
-import style from "./css/box-局部样式.module.css";
-import style2 from "./css/sass的使用.module.scss";
-import classNames from "classnames";
+// import style from "./css/box-局部样式.module.css";
+// import style2 from "./css/sass的使用.module.scss";
+// import classNames from "classnames";
 
-const classStyle = classNames({
-  [style.box ]: true,
-  [style2.box ]: true
-});
+// const classStyle = classNames({
+//   [style.box ]: true,
+//   [style2.box ]: true
+// });
+
+import { animated, useSpring } from "@react-spring/web";
+import { useState } from "react";
+
+function FadeIn({
+  isShow,
+  children,
+}: {
+  isShow: boolean;
+  children: React.ReactNode;
+}) {
+  const springs = useSpring({
+    from: { x: isShow ? 400 : 0, opacity: 0 },
+    to: { x: isShow ? 0 : 400, opacity: isShow ? 1 : 0 },
+  });
+  return <animated.div style={{ ...springs }}>{children}</animated.div>;
+}
 
 function App() {
+  const [isShow, setIsShow] = useState(false);
   return (
     <>
-      <div className={classStyle}>
-        你好
-        <span>我是span标签</span>
-      </div>
+      <button
+        onClick={() => setIsShow(!isShow)}
+        className="w-10 h-7 bg-gray-300 rounded-md m-2"
+      >
+        {isShow ? "隐藏" : "显示"}
+      </button>
+      <FadeIn isShow={isShow}>
+        <div className="bg-red-600 w-15  text-white text-center " >你好</div>
+      </FadeIn>
     </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import eslintPlugin from "vite-plugin-eslint";
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: 
-  [
-    react(), 
+  plugins: [
+    react(),
+    tailwindcss(),
     eslintPlugin({
-      include: ['src/**/*.tsx', 'src/*.tsx']
-    })
+      include: ["src/**/*.tsx", "src/*.tsx"],
+    }),
   ],
-})
+});
